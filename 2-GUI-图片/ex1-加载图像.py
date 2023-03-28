605a5a17f42594bd208ab2271a357ed7c0323ef2
ImCVER <ImCVER.niubi.com>
2023-03-28 23:12:16 +0800

    3.28更改

--- 2-GUI-图片/ex1-加载图像.py
+++ 2-GUI-图片/ex1-加载图像.py
@@ -7,6 +7,11 @@ img = cv2.imread('./images/butterfly.jpg', cv2.IMREAD_COLOR)
 # 3. 形状  (768, 1024, 3)  三通道  RGB
 # 形状  (768, 1024) 单通道，灰度图
 print(img.shape)
+
+
+
+
+
 # 4. img类型
 print(type(img))
 # 5. 显示图像
